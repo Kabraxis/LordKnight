af7a186cdd435bfe286470fb8c2358f4ad83c52f
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-12-27 02:23:26 +0100

    feature: new random for damage calculation

--- src/comgithubkabraxis/mechanics/Randomizer.java
+++ src/comgithubkabraxis/mechanics/Randomizer.java
@@ -3,8 +3,11 @@ package comgithubkabraxis.mechanics;
 import java.util.Random;
 
 public class Randomizer {
-
-    public static int randomRange(int min, int max) {
+    public static int rndRange(int min, int max) {
         return new Random().nextInt(min, max);
+    }
+
+    public static int rndDMG(int attack) {
+        return new Random().nextInt(1, attack);
     }
 }
