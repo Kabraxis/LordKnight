b4996547e29a24b93112850b7ad73f60ec46f8de
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-12-27 02:18:19 +0100

    feature: attack loop player
    The loop checks if any of the entities is already dead.
    A message is displayed for how much damage has been inflicted, and
    healthPreHit is now inside the loop, as it does not save any resources
    to have it outside, due to garbage collection.
    Damage is randomized each hit for a more vivid game experience.

--- src/comgithubkabraxis/mechanics/Battle.java
+++ src/comgithubkabraxis/mechanics/Battle.java
@@ -1,16 +1,20 @@
 package comgithubkabraxis.mechanics;
 
 import comgithubkabraxis.entities.*;
+import comgithubkabraxis.texts.Display;
 
 public class Battle {
     public static void fight(Player player, Monster monster) {
-        System.out.printf("%s encounters %s!%n%1$s's HP is: %d, %2$s's HP is: %d%nFIGHT !!",
+        System.out.printf("%s encounters %s!%n%1$s's HP is: %d, %2$s's HP is: %d%nCHARGE !!%n%n",
                 player.getName(), monster.getName(), player.getHealth(), monster.getHealth());
 
-        int healthPreHit;
-
-        while (player.getHealth() > 0 || monster.getHealth() >0) {
-
+        while (!player.isDead() || monster.isDead()) {
+            if (Randomizer.rndRange(0, Integer.MAX_VALUE) % 100 > monster.getAttack()) {
+               int healthPreHit = player.getHealth();
+                player.setHealth(player.getHealth() - (Randomizer.rndDMG(monster.getAttack())));
+                System.out.printf("%s%s%n",
+                        player.getName(), Display.hitMessage(healthPreHit, player.getHealth()));
+            }
         }
     }
 
