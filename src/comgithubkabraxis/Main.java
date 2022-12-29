13413f4fc13719907790ee6e557f800534b9fe03
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-12-29 21:14:44 +0100

    refactor: set Poring HP to 5, for testing

--- src/comgithubkabraxis/Main.java
+++ src/comgithubkabraxis/Main.java
@@ -6,7 +6,7 @@ import comgithubkabraxis.mechanics.Battle;
 public class Main {
     public static void main(String[] args) {
         Player kabraxis = new Player(100, 20, "Sir Kabraxis");
-        Monster poring = new Monster(50, 15, "Poring");
+        Monster poring = new Monster(5, 15, "Poring");
 
         Battle.fight(kabraxis, poring);
 
