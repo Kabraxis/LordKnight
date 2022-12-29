95bf3240016d2f1d23c1373639d6118e6f855d36
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-12-29 21:16:05 +0100

    feature: reworking Battle.fight

--- src/comgithubkabraxis/mechanics/Battle.java
+++ src/comgithubkabraxis/mechanics/Battle.java
@@ -9,14 +9,37 @@ public class Battle {
         System.out.printf("%s encounters %s!%n%1$s's HP is: %d, %2$s's HP is: %d%nCHARGE !!%n%n",
                 player.getName(), monster.getName(), player.getHealth(), monster.getHealth());
 
-        while (!player.isDead() || monster.isDead()) {
+        int healthPreHit;
+        while (!player.isDead() || !monster.isDead()) {
+            System.out.printf("Player is dead: %b  Monster is dead: %b%n",
+                    player.isDead(), monster.isDead());
             if (new Random().nextInt(0, Integer.MAX_VALUE) % 100 > monster.getAttack()) {
-               int healthPreHit = player.getHealth();
+                healthPreHit = player.getHealth();
                 player.setHealth(player.getHealth() - (new Random().nextInt(1, monster.getAttack())));
                 System.out.printf("%s%s%n",
                         player.getName(), Display.hitMessage(healthPreHit, player.getHealth()));
+            } else {
+                System.out.printf("%s%s%n",
+                        monster.getName(), Display.missedMessage());
             }
+            System.out.printf("Player is dead: %b  Monster is dead: %b%n",
+                    player.isDead(), monster.isDead());
         }
     }
-
 }
+//
+//            if (new Random().nextInt(0,Integer.MAX_VALUE) % 100 > player.getAttack()) {
+//                healthPreHit = monster.getHealth();
+//                monster.setHealth(monster.getHealth() - (new Random().nextInt(1, player.getAttack())));
+//                System.out.printf("%s%s%n",
+//                        monster.getName(), Display.hitMessage(healthPreHit, monster.getHealth()));
+//            } else {
+//                System.out.printf("You%s%n",
+//                        Display.missedMessage());
+//            }
+//        }
+//
+//        System.out.println(player.isDead() ? "You are dead!" : monster.getName() + " is dead!");
+
+
+
