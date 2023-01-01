c2e43dc0729038631158f06b1665cd3706801f7e
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-01-01 10:13:33 +0100

    refactor: move all texts to Display.java

--- src/mechanics/Battle.java
+++ src/mechanics/Battle.java
@@ -1,6 +1,7 @@
 package mechanics;
 
 import java.util.Random;
+
 import entities.Monster;
 import entities.Player;
 import texts.Display;
@@ -8,8 +9,8 @@ import texts.Display;
 public class Battle {
     public static void fight(Player player, Monster monster) {
 
-        System.out.printf("%s encounters %s!%n%1$s's HP is: %d, %2$s's HP is: %d%nCHARGE !!%n%n",
-                player.getName(), monster.getName(), player.getHealth(), monster.getHealth());
+        Display.encountering(player.getName(), monster.getName(),
+                player.getHealth(), monster.getHealth());
 
         int healthPreHit;
         while (player.getHealth() != 0 && monster.getHealth() != 0) {
@@ -17,21 +18,17 @@ public class Battle {
                 healthPreHit = player.getHealth();
 
                 player.setHealth(player.getHealth() - (new Random().nextInt(1, monster.getAttack())));
-                Display.hitMessage(player.getName(), healthPreHit, player.getHealth());
+                Display.hit(player.getName(), healthPreHit, player.getHealth());
             } else {
-                System.out.printf("%s%s%n",
-                        monster.getName(), Display.missedMessage());
+                Display.miss(monster.getName());
             }
 
             if (player.getHealth() <= 0) {
-                System.out.printf("%s is dead!%n",
-                        player.getName());
-
+                Display.deathnote(player.getName());
             }
 
             if (monster.getHealth() <= 0) {
-                System.out.printf("%s is dead!%n",
-                        monster.getName());
+                Display.deathnote(monster.getName());
             }
 
         }
--- src/texts/Display.java
+++ src/texts/Display.java
@@ -1,15 +1,28 @@
 package texts;
 
 public class Display {
-    public static void hitMessage(String entityName, int healthPreHit, int hitPoints) {
-        System.out.println(" got hit for: " + (healthPreHit - hitPoints) + " DMG!");
+    public static void hit(String entityName, int healthPreHit, int hitPoints) {
+        System.out.printf("%s got hit for: %d DMG!\"",
+                entityName, healthPreHit - hitPoints);
     }
 
-    public static String missedMessage() {
-        return " missed!";
+    public static void miss(String entityName) {
+        System.out.printf("%s missed!",
+                entityName);
     }
 
-    public static void selectAction() {
+    public static void selection() {
         System.out.println("Select action: 1 - fight, 2 - potion, 3 - run'\n'");
+    }
+
+    public static void encountering(String entityNameAttacker, String entityNameDefender,
+                                    int healthAttacker, int healthDefender) {
+        System.out.printf("%s encounters %s!%n%1$s's HP is: %d, %2$s's HP is: %d%nCHARGE !!%n%n",
+                entityNameAttacker, entityNameDefender, healthAttacker, healthDefender);
+    }
+
+    public static void deathnote(String entityNamenDefender) {
+        System.out.printf("%s is dead",
+                entityNamenDefender);
     }
 }
