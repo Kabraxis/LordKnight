cf2878c729911961980b9df6e14234303ffe7e33
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-01-01 10:17:11 +0100

    initial commit for Action.java

---
+++ src/mechanics/Action.java
@@ -0,0 +1,29 @@
+package mechanics;
+
+import java.util.Scanner;
+import texts.Display;
+/*
+Todo
+- selection must scan for player selection
+- input must be validated, decimal 1-3 only
+*/
+
+public class Action {
+    public static void selection() {
+        Scanner scanner = new Scanner(System.in);
+        Display.selection();
+        scanner.nextLine();
+    }
+
+    public static void run() {
+
+    }
+
+    public static void potion() {
+
+    }
+
+    public static void fight() {
+
+    }
+}
