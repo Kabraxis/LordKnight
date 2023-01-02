e0ff264b9d918dcf4cb2ff2e501f59aea95104ab
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-01-02 19:25:35 +0100

    feature: action is now decide, add decisions
    Decide now three options to choose from, redundant methods removed.

---
+++ src/mechanics/Decide.java
@@ -0,0 +1,35 @@
+package mechanics;
+
+/*
+Todo
+- selection must scan for player selection
+- input must be validated, decimal 1-3 only
+*/
+
+import texts.Display;
+
+public class Decide {
+    public static void selectMenu() {
+
+        //  Actions and therefore cases are: 1 - attack, 2 - potion, 3 - run
+        Display.actions();
+        switch (Validate.getIntInput()) {
+            case 1:
+                Battle.fight();
+                break;
+
+            case 2:
+                Item.potion();
+                break;
+
+            case 3:
+                Initiative.whoseCharge();
+                break;
+
+            default:
+                System.out.println("Ehhhhhhhhhhh!");
+
+        }
+
+    }
+}
