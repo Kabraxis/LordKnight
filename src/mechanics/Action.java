d8bdfbbe5e01601fb1af972d095e2e72df21702b
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-01-02 18:38:12 +0100

    refactor: Display action, selection
    selection is selectMenu, Display.actions() added

--- src/mechanics/Action.java
+++ src/mechanics/Action.java
@@ -1,16 +1,16 @@
 package mechanics;
 
-
-import texts.Display;
 /*
 Todo
 - selection must scan for player selection
 - input must be validated, decimal 1-3 only
 */
 
-public class Action {
-    public static void selection() {
+import texts.Display;
 
+public class Action {
+    public static void selectMenu() {
+        Display.actions();
 
     }
 
