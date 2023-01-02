55e24a0f35ad41d15e629aa8dd6d11f295a53ce5
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-01-02 18:41:51 +0100

    refactor: renaming

--- src/mechanics/Validate.java
+++ src/mechanics/Validate.java
@@ -4,13 +4,12 @@ import texts.Display;
 import java.util.Scanner;
 
 public class Validate {
-    public static int getAction() {
+    public static int getIntInput() {
         Scanner scanner = new Scanner(System.in);
-        Display.selection(); // 1 - attack, 2 - potion, 3 - run
 
         while (!scanner.hasNextInt(1 | 2 | 3)) {
             Display.invalidAction();
-            Display.selection();
+            Display.actions();
             scanner.next(); // clears scanner
         }
         return scanner.nextInt();
